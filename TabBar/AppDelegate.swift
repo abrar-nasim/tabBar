947ba7c483c0508c163d3ff82bdca06433537293
Md Abrar Nasim <abrarnsim555@gmail.com>
2023-03-27 11:10:21 +0530

    add map

--- TabBar/AppDelegate.swift
+++ TabBar/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GooglePlaces
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSPlacesClient.provideAPIKey("AIzaSyDjrTtBHfdo2ay8mlxUqZVHXO7cWjlXm44")
         return true
     }
 
--- TabBar/ViewController.swift
+++ TabBar/ViewController.swift
@@ -6,13 +6,67 @@
 //
 
 import UIKit
+import MapKit
+import GooglePlaces
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var resultsViewController: GMSAutocompleteResultsViewController?
+    var searchController: UISearchController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupSearchController()
+        resultsViewController?.delegate = self
     }
+
+    func setupSearchController() {
+        resultsViewController = GMSAutocompleteResultsViewController()
+        searchController = UISearchController(searchResultsController: resultsViewController)
+        searchController?.searchResultsUpdater = resultsViewController
+
+        let searchBar = searchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for places"
+        navigationItem.titleView = searchController?.searchBar
+        definesPresentationContext = true
+        searchController?.hidesNavigationBarDuringPresentation = false
+    }
+
+    
+}
+
+
+
+extension ViewController: GMSAutocompleteResultsViewControllerDelegate {
+    func resultsController(_ resultsController: GMSAutocompleteResultsViewController, didAutocompleteWith place: GMSPlace) {
+        // 1
+        searchController?.isActive = false
+
+        // 2
+        mapView.removeAnnotations(mapView.annotations)
+
+        // 3
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: place.coordinate, span: span)
+        mapView.setRegion(region, animated: true)
+
+        // 4
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = place.coordinate
+        annotation.title = place.name
+        annotation.subtitle = place.formattedAddress
+        mapView.addAnnotation(annotation)
+
+    }
+
+    func resultsController(_ resultsController: GMSAutocompleteResultsViewController, didFailAutocompleteWithError error: Error) {
+        print("Error: \(error.localizedDescription)")
+    }
+
 
 
 }
